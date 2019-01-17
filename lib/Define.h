fcb2cbd0f72d9f5495c50016c74582a7a089920b
HemingCHEN <chmkeily@gmail.com>
2019-01-17 16:06:57 +0800

    创建定义文件
    用于声明一致性模块的基础定义变量

---
+++ lib/Define.h
@@ -0,0 +1,7 @@
+/************************************************
+ *      Filename: Define.h
+ *   Descprition: RAFT consensus module definitions
+ *        Author: hemingchen@tencent.com
+ *       Created: 2019-01-18 16:05:16
+ ***********************************************/
+ 
