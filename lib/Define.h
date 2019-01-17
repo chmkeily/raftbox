917a4ef556dcd99ed65a3454d6a26672a1304c70
hemingchen(陈海明) <hemingchen@tencent.com>
2019-01-17 18:06:37 +0800

    Signed-off-by: hemingchen(陈海明) <hemingchen@tencent.com>

--- lib/Define.h
+++ lib/Define.h
@@ -4,4 +4,24 @@
  *        Author: hemingchen@tencent.com
  *       Created: 2019-01-18 16:05:16
  ***********************************************/
- 
+ #pragma once
+
+//RAFT一致性模块最大节点数
+#define RAFT_MAX_NODE_NUM   15
+
+//RAFT节点状态
+enum EnmRaftNodeState
+{
+    RAFT_NODE_STAT_LEADER       = 1,    //Leader节点
+    RAFT_NODE_STAT_CANDIDATE    = 2,    //候选人节点
+    RAFT_NODE_STAT_FOLLOWER     = 3,    //Follower节点
+}
+
+//RAFT日志状态
+enum EnmRaftLogState
+{
+    RAFT_LOG_STATE_REPLICATE    = 1,    //同步复制
+    RAFT_LOG_STATE_COMMITTED    = 2,    //已提交
+    RAFT_LOG_STATE_APPLIED      = 3,    //已应用
+}
+
